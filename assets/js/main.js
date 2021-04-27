c7eacda69b2830681f3e7d7d450e0e3f565776a6
fdrbrs <federio.borsci@gmail.com>
2021-04-27 14:20:08 +0200

    aggiunta stampa a schermo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 //Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+
+
 for (var i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
@@ -15,3 +17,25 @@ for (var i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+//stampo a schermo
+
+var array = [];
+
+
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        array.push("FizzBuzz");
+    } 
+    else if (i % 5 == 0) {
+        array.push("Buzz");
+    }
+    else if (i % 3 == 0){
+        array.push("Fizz");
+    }
+    else {
+        array.push(i);
+    }
+}
+
+document.getElementById("array").innerHTML = array;
